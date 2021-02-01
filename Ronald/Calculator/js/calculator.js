cd1e15f4077193d6e7b6431be652fc9bc3115c5d
unknown <DelaCruz.RonaldVergel@gmail.com>
2021-02-01 19:43:26 +0800

    BOOTSTRAP LESSON

--- Ronald/Calculator/js/calculator.js
+++ Ronald/Calculator/js/calculator.js
@@ -1,11 +1,3 @@
-var calcu = document.getElementsById("display");
-
-function inputOne(){
-    var one = document.getElementById("one").value;
-    display.value += one.value;
-}
-
-function clearInput(){
-    var clear = document.getElementById("clear");
-    display.value = '';
+function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
 }
